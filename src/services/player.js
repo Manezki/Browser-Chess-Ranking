93707e0d6def22d7308c7fcb18c5f7acc07092a2
Manezki <manezki@gmail.com>
2021-07-08 19:26:45 +0200

    Feat: Add updatePlayer endpoint to services

--- src/services/player.js
+++ src/services/player.js
@@ -19,3 +19,16 @@ export const addNew = ({ name }) => {
   localStorage.setItem('players', JSON.stringify(playersData.concat(newPlayer)))
   return newPlayer
 }
+
+export const updatePlayer = (id, { elo }) => {
+  const playersData = getAll()
+  const updatedPlayersData = playersData.map((oldPlayer) => {
+    return (oldPlayer.id === id) ? {
+      ...oldPlayer,
+      elo: elo || oldPlayer.elo,
+    } : oldPlayer
+  })
+
+  localStorage.setItem('players', JSON.stringify(updatedPlayersData))
+  return updatedPlayersData.find((player) => player.id === id)
+}
--- src/services/player.unit.test.js
+++ src/services/player.unit.test.js
@@ -35,4 +35,36 @@ describe('Player service', () => {
       expect(newPlayerLeft.id).not.toBe(newPlayerRight.id)
     })
   })
+
+  describe('updatePlayer', () => {
+    test('Updates a player in localStorage with matching id', () => {
+      window.localStorage.clear()
+      window.localStorage.setItem('players', JSON.stringify([testPlayer]))
+
+      const updatedPlayer = { ...testPlayer, elo: 1220 }
+      playerAPI.updatePlayer(updatedPlayer.id, { elo: 1220 })
+
+      expect(JSON.parse(window.localStorage.getItem('players'))[0]).toEqual(updatedPlayer)
+    })
+
+    test('Returns the updated player', () => {
+      window.localStorage.clear()
+      window.localStorage.setItem('players', JSON.stringify([testPlayer]))
+
+      const updatedPlayer = playerAPI.updatePlayer(testPlayer.id, { elo: 1220 })
+
+      expect(updatedPlayer.elo).toBe(1220)
+    })
+
+    test('Does not overwrite unupdated properties', () => {
+      window.localStorage.clear()
+      window.localStorage.setItem('players', JSON.stringify([testPlayer]))
+
+      playerAPI.updatePlayer(testPlayer.id, {})
+
+      expect(JSON.parse(window.localStorage.getItem('players'))[0].elo).toBe(1200)
+      expect(JSON.parse(window.localStorage.getItem('players'))[0].name).toBe('A')
+      expect(JSON.parse(window.localStorage.getItem('players'))[0].id).toBe(testPlayer.id)
+    })
+  })
 })
