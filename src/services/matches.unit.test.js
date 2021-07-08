a81a4baa80c815b0596a2ffec6a98999d95534f6
Manezki <manezki@gmail.com>
2021-07-08 14:18:59 +0200

    Test: Explicitly set timezone
    Addresses flaky testing in Github Actions

--- src/services/matches.unit.test.js
+++ src/services/matches.unit.test.js
@@ -8,7 +8,7 @@ jest.spyOn(window.localStorage.__proto__, 'setItem')
 jest.spyOn(window.localStorage.__proto__, 'getItem')
 
 const testMatch = {
-  datetime: Date.parse('01-01-2021'),
+  datetime: Date.parse('01-01-2021 00:00:00 GMT'),
   player1: { name: 'A', elo: 1200, id: 1 },
   player2: { name: 'B', elo: 1200, id: 2 },
   outcome: 'player1',
@@ -25,12 +25,12 @@ describe('Matches service', () => {
 
     test('Returned match has datetime, player1, player2, outcome, and ID defined', () => {
       const newMatch = matchesAPI.addNew({
-        datetime: Date.parse('01-01-2021'),
+        datetime: Date.parse('01-01-2021 00:00:00 GMT' ),
         player1: { name: 'A', elo: 1200, id: 1 },
         player2: { name: 'B', elo: 1200, id: 2 },
         outcome: 'player1'
       })
-      expect(newMatch.datetime).toBe(1609455600000)
+      expect(newMatch.datetime).toBe(1609459200000)
       expect(newMatch.player1).toEqual({ name: 'A', elo: 1200, id: 1 })
       expect(newMatch.player2).toEqual({ name: 'B', elo: 1200, id: 2 })
       expect(newMatch.outcome).toBe('player1')
@@ -39,13 +39,13 @@ describe('Matches service', () => {
 
     test('Generates a unique ID to the new players', () => {
       const newMatchLeft = matchesAPI.addNew({
-        datetime: Date.parse('01-01-2021'),
+        datetime: Date.parse('01-01-2021 00:00:00 GMT'),
         player1: { name: 'A', elo: 1200, id: 1 },
         player2: { name: 'B', elo: 1200, id: 2 },
         outcome: 'player1'
       })
       const newMatchRight = matchesAPI.addNew({
-        datetime: Date.parse('01-01-2021'),
+        datetime: Date.parse('01-01-2021 00:00:00 GMT'),
         player1: { name: 'A', elo: 1200, id: 1 },
         player2: { name: 'B', elo: 1200, id: 2 },
         outcome: 'player1'
